f3163055c664a6a71d39f22dac8af0a7d20e0b89
Daniel Tieber <daniel.tieber@dynatrace.com>
2023-02-09 10:30:27 +0100

    feat(ctc): use provider to make store available and extract app to own component

---
+++ packages/content-types-client/src/app.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+
+export function App(): JSX.Element {
+  return <div>
+    <p>will add components here</p>
+  </div>
+}
--- packages/content-types-client/src/index.tsx
+++ packages/content-types-client/src/index.tsx
@@ -1,8 +1,16 @@
 import * as React from 'react'
 import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+
+import { App } from './app'
+import { store } from './app/store'
 
 const container = document.getElementById('app-root')
 if(container) {
   const root = createRoot(container)
-  root.render(<h1>Hello World!</h1>)
+  root.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+  )
 }
