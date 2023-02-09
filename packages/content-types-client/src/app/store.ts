5a66dcc7a11206beb26dbd735f96df3ce6919dd7
Daniel Tieber <daniel.tieber@dynatrace.com>
2023-02-09 14:55:13 +0100

    feat(ctc): initialise saga middleware with root-saga and root-reducer

--- packages/content-types-client/src/app/store.ts
+++ packages/content-types-client/src/app/store.ts
@@ -1,8 +1,20 @@
 import { configureStore } from '@reduxjs/toolkit'
+import createSagaMiddleware from 'redux-saga'
+
+import { rootReducer } from '../features/root-reducer'
+import { rootSaga } from '../features/root-saga'
+
+const sagaMiddleware = createSagaMiddleware()
+const middlewares = [
+  sagaMiddleware,
+]
 
 export const store = configureStore({
-  reducer: {},
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({ thunk:false }).concat(middlewares),
+  reducer: rootReducer,
 })
+
+sagaMiddleware.run(rootSaga)
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
