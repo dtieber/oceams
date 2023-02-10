cb6f1a21c8f7c95f9bb86283b02cbdc950823fba
Daniel Tieber <daniel.tieber@dynatrace.com>
2023-02-10 09:10:38 +0100

    feat(ctc): enable strict mode for React

--- packages/content-types-client/src/index.tsx
+++ packages/content-types-client/src/index.tsx
@@ -10,7 +10,9 @@ if(container) {
   const root = createRoot(container)
   root.render(
     <Provider store={store}>
-      <App />
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
     </Provider>,
   )
 }
