78478d68374265419a99ca95e83f5cfbd50bb9b4
Daniel Tieber <daniel.tieber@dynatrace.com>
2023-02-10 08:44:55 +0100

    feat(ctc): add hooks for contextualized usage of selectors and dispatching actions

---
+++ packages/content-types-client/src/app/hooks.ts
@@ -0,0 +1,8 @@
+import type { TypedUseSelectorHook } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
+
+import type { AppDispatch, RootState } from './store'
+
+type DispatchFunc = () => AppDispatch
+export const useAppDispatch: DispatchFunc = useDispatch
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
