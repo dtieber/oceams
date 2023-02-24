95260fc82966b7e09bafe7f384be1e15ad5573af
Daniel Tieber <daniel.tieber@dynatrace.com>
2023-02-24 12:01:06 +0100

    feat(ctc): connect `addProperty` action to component

--- packages/content-types-client/src/features/content-type-builder/content-type-builder.container.ts
+++ packages/content-types-client/src/features/content-type-builder/content-type-builder.container.ts
@@ -2,12 +2,15 @@ import { connect } from 'react-redux'
 
 import type { RootState } from '../../app/store'
 import { ContentTypeBuilder } from './content-type-builder'
-import { setName } from './content-type-builder.actions'
+import { addProperty, setName } from './content-type-builder.actions'
 import type { ContentTypeBuilderState } from './content-type-builder.reducers'
 
 const mapStateToProps = (state: RootState): ContentTypeBuilderState => ({ ...state.contentTypeBuilder })
 
-const mapDispatch = { setName }
+const mapDispatch = {
+  addProperty,
+  setName,
+}
 
 export const ContentTypeBuilderContainer = connect(
   mapStateToProps,
