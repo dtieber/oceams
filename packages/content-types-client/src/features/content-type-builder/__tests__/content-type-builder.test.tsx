d3a9fec38b07d32a3f5807a9d057f98868547d52
Daniel Tieber <daniel.tieber@dynatrace.com>
2023-03-01 14:35:38 +0100

    feat(ctc): add AddProperty component to content-type-builder
    the `beforeAll` block is needed because JSDOM does not provide `window.matchMedia`. This is the official workaround: https://jestjs.io/docs/manual-mocks#mocking-methods-which-are-not-implemented-in-jsdom

--- packages/content-types-client/src/features/content-type-builder/__tests__/content-type-builder.test.tsx
+++ packages/content-types-client/src/features/content-type-builder/__tests__/content-type-builder.test.tsx
@@ -1,26 +1,64 @@
-import { describe, expect, it, jest } from '@jest/globals'
+import { beforeAll, describe, expect, it, jest } from '@jest/globals'
 import { fireEvent, render, screen } from '@testing-library/react'
 import * as React from 'react'
 
 import { ContentTypeBuilder } from '../content-type-builder'
-import type { SetNameAction } from '../content-type-builder.action-types'
+import type { AddPropertyAction, SetNameAction } from '../content-type-builder.action-types'
 
 describe('content-type-builder', () => {
+  beforeAll(() => {
+    Object.defineProperty(window, 'matchMedia', {
+      writable: true,
+      value: jest.fn().mockImplementation(query => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(),
+        removeListener: jest.fn(),
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+      })),
+    })
+  })
+
   it('displays name of content type', () => {
     const mockSetName = (_:string): SetNameAction => ({} as SetNameAction)
+    const mockAddProperty = (_:string): AddPropertyAction => ({} as AddPropertyAction)
 
-    render(<ContentTypeBuilder name='FooContentType' setName={mockSetName} />)
-    
+    render(<ContentTypeBuilder name='FooContentType' setName={mockSetName} properties={[]} addProperty={mockAddProperty} />)
+
     expect(screen.getByRole('heading')?.textContent).toBe('FooContentType')
   })
 
   it('updates name of content-type', () => {
     const mockSetName = jest.fn() as (_: string) => SetNameAction
+    const mockAddProperty = (_:string): AddPropertyAction => ({} as AddPropertyAction)
 
-    const component = render(<ContentTypeBuilder name='FooContentType' setName={mockSetName} />)
+    const component = render(<ContentTypeBuilder name='FooContentType' setName={mockSetName} properties={[]} addProperty={mockAddProperty} />)
     const input = component.getByPlaceholderText('Name of your content type')
     fireEvent.change(input, { target: { value: 'NewComponentName' } })
 
     expect(mockSetName).toHaveBeenCalledWith('NewComponentName')
+  })
+
+  it('displays a list of all properties', () => {
+    const mockSetName = jest.fn() as (_: string) => SetNameAction
+    const mockAddProperty = (_:string): AddPropertyAction => ({} as AddPropertyAction)
+    const listOfProperties = [
+      {
+        name: 'my-property-name',
+        propertyType: 'string',
+      },
+      {
+        name: 'my-property-name2',
+        propertyType: 'string',
+      },
+    ]
+
+    const component = render(<ContentTypeBuilder name='FooContentType' setName={mockSetName} properties={listOfProperties} addProperty={mockAddProperty} />)
+
+    expect(component.getByText('my-property-name')).toBeDefined()
+    expect(component.getByText('my-property-name2')).toBeDefined()
   })
 })
--- packages/content-types-client/src/features/content-type-builder/content-type-builder.tsx
+++ packages/content-types-client/src/features/content-type-builder/content-type-builder.tsx
@@ -1,12 +1,23 @@
-import { Input, Typography } from 'antd'
+import { Input, List, Typography } from 'antd'
 import * as React from 'react'
 
+import { AddProperty } from './add-property'
 import type { SetNameAction } from './content-type-builder.action-types'
-import type { setName } from './content-type-builder.actions'
+import type { addProperty, setName } from './content-type-builder.actions'
 import type { ContentTypeBuilderState } from './content-type-builder.reducers'
+import { getUniquePropertyName } from './get-unique-property-name'
+import type { PropertyType } from './property-types'
 
-export function ContentTypeBuilder(props: ContentTypeBuilderState & { setName: typeof setName}): JSX.Element {
-  const { name, setName } = props
+type ContentTypeBuilderProps = ContentTypeBuilderState & { setName: typeof setName} & { addProperty: typeof addProperty}
+
+export function ContentTypeBuilder(props: ContentTypeBuilderProps): JSX.Element {
+  const { name, setName, properties, addProperty } = props
+
+  const handleOnAddProperty = (type: PropertyType): void => {
+    const uniquePropertyName = getUniquePropertyName(properties, type)
+    addProperty(uniquePropertyName, type)
+  }
+
   return <div>
     <Typography>
       <Typography.Title>{name}</Typography.Title>
@@ -15,6 +26,12 @@ export function ContentTypeBuilder(props: ContentTypeBuilderState & { setName: t
       placeholder='Name of your content type'
       value={name}
       onChange={(e): SetNameAction => setName(e.target.value)}/>
+    <AddProperty onAddProperty={handleOnAddProperty} />
+    <List
+      size='small'
+      dataSource={properties}
+      renderItem={(item):JSX.Element => <List.Item>{item.name}</List.Item>}>
+    </List>
   </div>
 
 }
