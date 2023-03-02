bd33a5836dbad1ddc27a0b1dfdf1523b84e3ac10
Daniel Tieber <daniel.tieber@dynatrace.com>
2023-03-02 14:58:48 +0100

    refactor(ctc): put reducers in alphabetical order

--- packages/content-types-client/src/features/content-type-builder/__tests__/content-type-builder.reducers.test.ts
+++ packages/content-types-client/src/features/content-type-builder/__tests__/content-type-builder.reducers.test.ts
@@ -4,32 +4,6 @@ import { addProperty, setName } from '../content-type-builder.actions'
 import { reducers, selectContentTypeName, selectProperties } from '../content-type-builder.reducers'
 
 describe('content-type-builder.reducers', () => {
-  it('SET_NAME action updates the name', () => {
-    const originalState = {
-      name: 'oldName',
-      properties: [],
-    }
-
-    const setNameAction = setName('newName')
-    const updatedState = reducers(originalState, setNameAction)
-
-    expect(updatedState).toEqual({
-      name: 'newName',
-      properties: [],
-    })
-  })
-
-  it('selectContentTypeName selector returns name of content type', () => {
-    const rootState = {
-      contentTypeBuilder: {
-        name: 'foo',
-        properties: [],
-      },
-    }
-
-    expect(selectContentTypeName(rootState)).toEqual('foo')
-  })
-
   it('ADD_PROPERTY action adds a property to an empty array of properties', () => {
     const originalState = {
       name: 'my-name',
@@ -98,5 +72,31 @@ describe('content-type-builder.reducers', () => {
         propertyType: 'string',
       },
     ])
+  })
+
+  it('SET_NAME action updates the name', () => {
+    const originalState = {
+      name: 'oldName',
+      properties: [],
+    }
+
+    const setNameAction = setName('newName')
+    const updatedState = reducers(originalState, setNameAction)
+
+    expect(updatedState).toEqual({
+      name: 'newName',
+      properties: [],
+    })
+  })
+
+  it('selectContentTypeName selector returns name of content type', () => {
+    const rootState = {
+      contentTypeBuilder: {
+        name: 'foo',
+        properties: [],
+      },
+    }
+
+    expect(selectContentTypeName(rootState)).toEqual('foo')
   })
 })
