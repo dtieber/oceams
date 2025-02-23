77e1e8c4030beee13182560a822bbb93bcc897d9
Daniel Tieber <daniel.tieber@dynatrace.com>
2025-02-23 20:01:38 +0100

    fix(ctc): add missing `setDomain` action to dispatchers

--- packages/content-types-client/src/features/content-type-builder/content-type-builder.container.ts
+++ packages/content-types-client/src/features/content-type-builder/content-type-builder.container.ts
@@ -2,13 +2,14 @@ import { connect } from 'react-redux'
 
 import type { RootState } from '../../app/store'
 import { ContentTypeBuilder } from './content-type-builder'
-import { addProperty, setName } from './content-type-builder.actions'
+import { addProperty, setDomain, setName } from './content-type-builder.actions'
 import type { ContentTypeBuilderState } from './content-type-builder.reducers'
 
 const mapStateToProps = (state: RootState): ContentTypeBuilderState => ({ ...state.contentTypeBuilder })
 
 const mapDispatch = {
   addProperty,
+  setDomain,
   setName,
 }
 
